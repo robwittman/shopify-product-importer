bf566de65cac6eeea34b7b6388abe82407807510
Rob Wittman <rob@ihsdigital.com>
2016-12-20 18:19:49 -0500

    Initial Commit

--- web/index.php
+++ web/index.php
@@ -5,9 +5,19 @@ require_once('../vendor/autoload.php');
 $app = new Slim\App(array(
     'settings' => $settings
 ));
+$container = $app->getContainer();
+$container['view'] = function($c) {
+    $view = new \Slim\Views\Twig('../views');
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath(), '/'));
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
 
 $app->get('/', function ($request, $response) {
-    return $response->withJson(array('success' => true));
+    return $this->view->render($response, 'profile.html', [
+        'name' => 'Rob Wittman'
+    ]);
 });
 
 $app->run();
