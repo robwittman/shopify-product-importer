50b4ac1a088c0df4d2655eea8bf5524ca76a9ef5
Rob Wittman <rob@ihsdigital.com>
2016-12-28 12:46:10 -0500

    Enabling user initialization

--- src/routes.php
+++ src/routes.php
@@ -10,14 +10,14 @@ $app->get('/', function ($request, $response) {
 #
 #   Uncomment to tu admin user in database
 #
-// $app->get('/init', function ($request, $response) {
-//     $user = new \App\Model\User();
-//     $user->email = 'admin@admin.com';
-//     $user->password = 'password';
-//     $user->role = 'admin';
-//     $user->save();
-//     var_dump($user);
-// });
+$app->get('/init', function ($request, $response) {
+    $user = new \App\Model\User();
+    $user->email = 'admin@admin.com';
+    $user->password = 'password';
+    $user->role = 'admin';
+    $user->save();
+    var_dump($user);
+});
 /*========================================
     User Routes
  =======================================*/
