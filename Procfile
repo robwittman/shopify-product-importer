57edb20e18d8e8b01b01e355044689b49b98f0c5
Rob Wittman <rob@ihsdigital.com>
2017-01-13 07:15:30 -0500

    Adding background worker

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: vendor/bin/heroku-php-apache2 html/
+worker: php bin/worker.php
---
+++ bin/worker.php
@@ -0,0 +1,23 @@
+<?php
+// TODO: Get rabbitmq connection
+
+$connection = null;
+$channel = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
+error_log("Worker ready for messages");
+
+$callback = function($msg) {
+    error_log($msg);
+}
+
+$channel->basic_qos(null, 1 null);
+$channel->basic_consume('task_queue','',false,false,false,false,$callback)
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
