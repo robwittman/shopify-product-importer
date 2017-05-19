70346a45cf5f219f9b5578089f34e988cb2478d8
Rob Wittman <rob@ihsdigital.com>
2017-05-19 14:05:05 -0400

    Fixed SKU generation

--- bin/worker.php
+++ bin/worker.php
@@ -568,7 +568,7 @@ function createTumbler($queue)
                     "requires_shipping" => true,
                     "inventory_management" => null,
                     "inventory_policy" => "deny",
-                    "sku" => "TX (UV PRINTED) - T{$size}- {$color} - {$post['product_title']} {$size}oz"
+                    "sku" => "TX - T{$size}- {$color} - {$post['product_title']} {$size}oz"
                 );
                 if($color == 'Navy' && $size == '30') {
                     error_log("Moving $color / $size to front of array");
@@ -694,7 +694,7 @@ function createUvTumbler($queue)
                     "requires_shipping" => true,
                     "inventory_management" => null,
                     "inventory_policy" => "deny",
-                    "sku" => "TX - T{$size}- {$color} - {$post['product_title']} {$size}oz"
+                    "sku" => "TX (UV PRINTED) - T{$size}- {$color} - {$post['product_title']} {$size}oz"
                 );
                 if($color == 'Navy' && $size == '30') {
                     error_log("Moving $color / $size to front of array");
