59cb4253b4e6169b8e0337e479f9aa0e52f2835e
Rob Wittman <rob@ihsdigital.com>
2017-08-24 11:26:56 -0700

    Add UV drinkware

--- bin/worker.php
+++ bin/worker.php
@@ -47,12 +47,6 @@ while (true) {
             // $q->start();
             $data = json_decode($q->data, true);
             switch ($data['post']['template']) {
-                case 'tumbler':
-                    $res = createTumbler($q);
-                    break;
-                case 'uv_tumbler':
-                    $res = createUvTumbler($q);
-                    break;
                 case 'hats':
                     $res = createHats($q);
                     break;
@@ -67,6 +61,15 @@ while (true) {
                     break;
                 case 'uv_drinkware':
                     $res = createUvDrinkware($q);
+                    break;
+                case 'flasks':
+                    $res = createFlasks($q);
+                    break;
+                case 'baby_body_suit':
+                    $res = createBabyBodySuit($q);
+                    break;
+                case 'raglans':
+                    $res = createRaglans($q);
                     break;
                 default:
                     throw new \Exception("Invalid template {$data['post']['template']} provided");
@@ -101,6 +104,147 @@ function getImages($s3, $prefix) {
     return array_map(function($object) {
         return $object["Key"];
     }, $res);
+}
+
+function createUvDrinkware($queue)
+{
+    $prices = array(
+        '30' => '39.99',
+        '20' => '34.99'
+    );
+
+    global $s3;
+    $queue->started_at = date('Y-m-d H:i:s');
+    $data = json_decode($queue->data, true);
+    $post = $data['post'];
+    $shop = \App\Model\Shop::find($post['shop']);
+    $image_data = getImages($s3, $data['file']);
+    $imageUrls = [];
+    switch($shop->myshopify_domain) {
+        case 'piper-lou-collection.myshopify.com':
+        case 'plcwholesale.myshopify.com':
+        case 'importer-testing.myshopify.com':
+            $html = "<meta charset='utf-8' />
+                    <ul>
+                    <li>2x heat &amp; cold retention (compared to plastic tumblers).</li>
+                    <li>Double-walled vacuum insulation - Keeps Hot and Cold. </li>
+                    <li>Fits most cup holders, Clear lid to protect from spills. </li>
+                    <li>Sweat Free Design allows for a Strong Hold. </li>
+                    <li>These tumblers will ship separately from our distributor in Texas. </li>
+                    </ul>";
+            break;
+        case 'hopecaregive.myshopify.com':
+            $html = '<p><img src="https://cdn.shopify.com/s/files/1/1255/4519/files/16128476_220904601702830_291172195_n.jpg?9775130656601803865"></p><p>Designed, printed, and shipped in the USA!</p>';
+            break;
+        case 'game-slave.myshopify.com':
+            $html = '<p><img src="https://cdn.shopify.com/s/files/1/1066/2470/files/TC_Best_seller.jpg?v=1486047696"></p><p>Designed, printed, and shipped in the USA!</p>';
+            break;
+        default:
+            $html = '<p></p>';
+    }
+    foreach ($image_data as $name) {
+        $productData = pathinfo($name)['filename'];
+        $specs = explode('_-_', $productData);
+        $size = $specs[0];
+        $color = $specs[1];
+        $imageUrls[$size][$color] = $name;
+    }
+    $tags = explode(',', trim($post['tags']));
+    $tags = implode(',', $tags);
+    $product_data = array(
+        'title' => $post['product_title'],
+        'body_html' => $html,
+        'tags' => $tags,
+        'vendor' => 'Tx Tumbler',
+        'options' => array(
+            array(
+                'name' => "Size"
+            ),
+            array(
+                'name' => "Color"
+            )
+        ),
+        'variants' => array(),
+        'images' => array()
+    );
+
+    foreach ($imageUrls as $size => $colors) {
+        foreach ($colors as $color => $url) {
+            switch ($size) {
+                case '30':
+                    $option1 = '30oz Tumbler';
+                    $sku = "TX (UV PRINTED) - T30 - {$color} - {$post['product_title']} 30oz";
+                    break;
+                case '20':
+                    $option1 = '20oz Tumbler';
+                    $sku = "TX (UV PRINTED) - T20 - {$color} - {$post['product_title']} 20oz";
+                    break;
+            }
+            error_log($sku);
+            $variantData = array(
+                'title' => $option1. ' / '.$color,
+                'price' => $prices[$size],
+                'option1' => $option1,
+                'option2' => str_replace('_', ' ', $color),
+                'weight' => '1.1',
+                'weight_unit' => 'lb',
+                'requires_shipping' => true,
+                'inventory_management' => null,
+                'inventory_policy' => 'deny',
+                'sku' => $sku
+            );
+            if ($color == 'Navy' && $size == '30') {
+                $product_data['variants'] = array_merge(array($variantData), $product_data['variants']);
+            } else {
+                $product_data['variants'][] = $variantData;
+            }
+        }
+    }
+    $res = callShopify($shop, '/admin/products.json', 'POST', array(
+        'product' => $product_data
+    ));
+    $imageUpdate = array();
+    foreach ($res->product->variants as $variant) {
+        $size = $variant->option1;
+        $color = str_replace(' ', '_', $variant->option2);
+        switch ($size) {
+            case '30oz Tumbler':
+                $size = '30';
+                break;
+            case '20oz Tumbler':
+                $size = '20';
+                break;
+        }
+        $image = array(
+            'src' => "https://s3.amazonaws.com/shopify-product-importer/{$imageUrls[$size][$color]}",
+            'variant_ids' => array($variant->id)
+        );
+        $imageUpdate[] = $image;
+    }
+    $res = callShopify($shop, "/admin/products/{$res->product->id}.json", "PUT", array(
+        "product" => array(
+            'id' => $res->product->id,
+            'images' => $imageUpdate
+        )
+    ));
+
+    $queue->finish(array($res->product->id));
+    return array($res->product->id);
+}
+
+function createFlasks($queue)
+{
+
+}
+
+function createBabyBodySuit($queue)
+{
+
+}
+
+function createRaglans($queue)
+{
+
 }
 
 function createDrinkware($queue)
