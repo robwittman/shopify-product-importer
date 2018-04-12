2b5b9f1f28cdd1b0d659fba0510deb3f2ec227d5
Rob Wittman <rob@ihsdigital.com>
2018-04-11 19:38:35 -0700

    utf8 encode descriptions

--- src/Model/Setting.php
+++ src/Model/Setting.php
@@ -7,4 +7,15 @@ class Setting extends Elegant
     protected $table = 'template_settings';
 
     protected $fillable = array('shop_id', 'template_id');
+
+    public function setDescriptionAttribute($html)
+    {
+        $this->attributes['description'] = utf8_encode($html);
+        return $this;
+    }
+
+    public function getDescriptionAttribute($html)
+    {
+        return utf8_decode($html);
+    }
 }
--- src/Model/Shop.php
+++ src/Model/Shop.php
@@ -8,4 +8,15 @@ class Shop extends Elegant
     {
         return $this->belongsToMany(User::class, 'users_shops', 'shop_id', 'user_id');
     }
+
+    public function setDescriptionAttribute($html)
+    {
+        $this->attributes['description'] = utf8_encode($html);
+        return $this;
+    }
+
+    public function getDescriptionAttribute($html)
+    {
+        return utf8_decode($html);
+    }
 }
