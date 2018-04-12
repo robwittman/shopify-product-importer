f6977218928cc29d7da5568afde369c3a65c9c9f
Rob Wittman <rob@ihsdigital.com>
2018-04-11 17:15:10 -0700

    Default correct UV tumbler

--- bin/scripts/UVDrinkware.php
+++ bin/scripts/UVDrinkware.php
@@ -87,7 +87,7 @@ function createUvDrinkware($queue)
                 'inventory_policy' => 'deny',
                 'sku' => $sku
             );
-            if ($color == 'Navy' && $size == '30') {
+            if ($color == 'Black' && $size == '30') {
                 $product_data['variants'] = array_merge(array($variantData), $product_data['variants']);
             } else {
                 $product_data['variants'][] = $variantData;
