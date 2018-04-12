474db74a3b5ae631be8967eda1c57f721ed27128
Rob Wittman <rob@ihsdigital.com>
2018-04-11 17:20:55 -0700

    Lets add supervisor

--- deploy.php
+++ deploy.php
@@ -58,8 +58,8 @@ task('db:migrate', function() {
 
 task('deploy:supervisor', function() {
     run('crontab {{current_path}}/conf/crontab');
-    // run('cp {{current_path}}/conf/supervisor/* /etc/supervisor/conf.d/');
-    // run('service supervisor restart');
+    run('cp {{current_path}}/conf/supervisor/* /usr/supervisor/conf.d/');
+    run('supervisorctl reread && supervisorctl update');
 });
 // [Optional] If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
