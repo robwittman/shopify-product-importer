1c1054199d6e16e9ba0c1cc08d06ba78f6237814
Robert Wittman <robert.wittman@webpt.com>
2018-04-24 06:51:27 -0700

    Fix product type parser

--- bin/worker.php
+++ bin/worker.php
@@ -248,7 +248,7 @@ function getProductSettings(Shop $shop, Queue $queue, Template $template, Settin
         'title' => $queue->title,
         'body_html' => $queue->description ?: $setting->description ?: $shop->description ?: $template->description,
         'tags' => $tags,
-        'product_type' => $queue->product_type ?: $setting->product_type ?: $template->product_Type,
+        'product_type' => $queue->product_type ?: $setting->product_type ?: $template->product_type,
         'vendor' => $queue->vendor ?: $setting->vendor ?: $template->vendor,
         'variants' => array(),
         'images' => array()
