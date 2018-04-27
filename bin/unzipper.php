74e234608786f2a7f1d27720a220c7ce73deb827
Robert Wittman <robert.wittman@webpt.com>
2018-04-27 06:09:16 -0700

    Stashing

--- bin/unzipper.php
+++ bin/unzipper.php
@@ -36,26 +36,3 @@ while (true) {
         error_log($e->getMessage());
     }
 }
-
-
-// try {
-//     $result = $client->receiveMessage(array(
-//         'AttributeNames' => ['SentTimestamp'],
-//         'MaxNumberOfMessages' => 1,
-//         'MessageAttributeNames' => ['All'],
-//         'QueueUrl' => $queueUrl, // REQUIRED
-//         'WaitTimeSeconds' => 0,
-//     ));
-//     if (count($result->get('Messages')) > 0) {
-//         var_dump($result->get('Messages')[0]);
-//         $result = $client->deleteMessage([
-//             'QueueUrl' => $queueUrl, // REQUIRED
-//             'ReceiptHandle' => $result->get('Messages')[0]['ReceiptHandle'] // REQUIRED
-//         ]);
-//     } else {
-//         echo "No messages in queue. \n";
-//     }
-// } catch (AwsException $e) {
-//     // output error message if fails
-//     error_log($e->getMessage());
-// }
